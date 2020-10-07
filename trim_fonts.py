a9dc4b9c92e406eae211dadcf5c286308c89fd88
Michał Sidor (Michcioperz) <public+git@meekchopp.es>
2020-10-07 13:04:26 +0200

    Rule out files not of honour earlier for better font size

--- trim_fonts.py
+++ trim_fonts.py
@@ -16,16 +16,16 @@ glyphs = set()
 for content_file in all_files():
     if os.path.isfile(content_file):
         print(content_file)
-        with codecs.open(content_file, encoding='utf-8', errors='replace') as f:
-            glyphs |= set(f.read())
+        try:
+            with codecs.open(content_file, encoding='utf-8', errors='strict') as f:
+                glyphs |= set(f.read())
+        except UnicodeDecodeError:
+            pass
 
 for font_path in glob.glob("themes/m14n/assets/fonts/literata/variable/*.ttf"):
     font = fontforge.open(font_path)
     for glyph in glyphs:
-        try:
-            font.selection[ord(glyph)] = True
-        except ValueError:
-            pass
+        font.selection[ord(glyph)] = True
     font.selection.invert()
     for glyph in font.selection.byGlyphs:
         font.removeGlyph(glyph)
